d8fc43ee6ccb7880588dc57c8e44a950a62b1a39
ibi-khannn <146767915+ibi-khannn@users.noreply.github.com>
2023-11-10 07:29:23 -0500

    Project Fully Complete Added functionality to remove books, add new books using a form, and change the read status of books

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const containerDiv = document.querySelector(".container");
 const openButton = document.querySelector("[data-open-modal]");
 const closeButton = document.querySelector("[data-close-modal]");
 const modal = document.querySelector("[data-modal]");
+const submitBtn = document.querySelector("#submit-button");
+let bookCounter = 0; // to keep track of how many book objects have been created
 
 openButton.addEventListener("click", () => {
     modal.showModal();
@@ -19,39 +21,59 @@ function Book (name, author, pageCount, readStatus) {
     this.readStatus = readStatus;
 }
 
+submitBtn.addEventListener("click", () => {
+    const bookName = document.querySelector("#book-name").value;
+    const authorName = document.querySelector("#author-name").value;
+    const bookPages = document.querySelector("#page-count").value;
+    const bookStatus = document.getElementsByName("read-status");
+    let bookRead;
+    
+    for (i = 0; i < bookStatus.length; i++) {
+        if (bookStatus[i].checked) {
+            bookRead = bookStatus[i].value;
+        }
+    }
+    
+    const bookObject = new Book (bookName, authorName, bookPages, bookRead);
+    myLibrary.push(bookObject);
+    displayBooks();
+    bookCounter++;
+    
+})
+
 Book.prototype.info = function () { 
-    return this.name + "\n" + this.author + "\n" + this.pageCount + "\n" + this.readStatus + "\n";
+    return this.name + "\n" + this.author + "\n" + this.pageCount; // using this keyword to access the properties in the current object
 }
 
-function addBookToLibrary() {
-    const bookOne = new Book ("Hero", "bookAuthor", "bookPageCount", "bookReadStatus");
-    const bookTwo = new Book ("The Hobbit", "bookAuthor", "bookPageCount", "bookReadStatus");
-    const bookThree = new Book ("Great Expectations", "bookAuthor", "bookPageCount", "bookReadStatus");
-    const bookFour = new Book ("Famous Five", "bookAuthor", "bookPageCount", "bookReadStatus");
-    myLibrary.push(bookOne);
-    myLibrary.push(bookTwo);
-    myLibrary.push(bookThree);
-    myLibrary.push(bookFour);
+Book.prototype.status = function () { // created separate method so that the read status can be toggled on the browser using textContent in toggleReadStatus button
+    return this.readStatus;
 }
-
-addBookToLibrary();
-displayBooks ();
 
 function displayBooks () {
     let j = 0;
-    for (i = 0; i < myLibrary.length; i++) {
+    
+    for (let i = bookCounter; i < myLibrary.length; i++) {
         const bookCard = document.createElement("div");
         bookCard.classList.add("book-card");
         containerDiv.appendChild(bookCard);
         bookCard.textContent = myLibrary[i].info();
+        const readBook = document.createElement("p");
+        readBook.textContent = myLibrary[i].status();
+        readBook.style.margin = "0px";
+        bookCard.appendChild(readBook);
         const removeBookBtn = document.createElement("button");
         removeBookBtn.textContent = "Remove Book";
         removeBookBtn.style.marginTop = "7%";
         bookCard.appendChild(removeBookBtn);
+        const toggleReadStatus = document.createElement("button");
+        toggleReadStatus.textContent = "Change Read Status";
+        toggleReadStatus.style.marginTop = "7%";
+        bookCard.appendChild(toggleReadStatus);
         bookCard.setAttribute("data-index", i);
+        
         removeBookBtn.addEventListener ("click", () => {
             const indexNumber =  bookCard.dataset.index;
-            myLibrary.splice(indexNumber, 1);
+            myLibrary.splice(indexNumber, 1); // remove that card object from the myLibrary array when pressed
             containerDiv.removeChild(bookCard);
             console.log(myLibrary);
             const allBookCards = document.querySelectorAll(".book-card");
@@ -60,7 +82,24 @@ function displayBooks () {
                 eachBook.setAttribute("data-index", j);
                 j++;
             })
-            j = 0;
+            j = 0; // set j = 0 once again so that when the remove button is pressed then the data-index is reassigned again after removal
+            bookCounter = bookCounter - 1; // decrements bookcounter by 1 when Remove Button is clicked so that New books can keep being added
+
+        })
+        
+        toggleReadStatus.addEventListener ("click", () => {
+            const indexNumber = bookCard.dataset.index;
+            
+            if (myLibrary[indexNumber].readStatus == "Yes") { // change read status of the clicked book object
+                myLibrary[indexNumber].readStatus = "No";
+            }
+            else if (myLibrary[indexNumber].readStatus == "No") {
+                myLibrary[indexNumber].readStatus = "Yes";
+            }
+
+            readBook.textContent = myLibrary[indexNumber].status(); // change the read status on the browser
+
+            console.log(myLibrary);
         })
     }
 }
